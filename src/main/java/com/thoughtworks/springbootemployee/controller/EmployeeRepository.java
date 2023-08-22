259883b0761672d5ae563af456e8d02480106e73
Daphne Balane <balanedaphne20@gmail.com>
2023-08-22 15:53:50 +0800

    feat: added methods in EmployeeRepository class

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeRepository.java
@@ -0,0 +1,34 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Repository
+public class EmployeeRepository {
+    private static final List<Employee> employees = new ArrayList<>();
+
+    static {
+        employees.add(new Employee(1, "Daphne", 23, "Female", 1000));
+        employees.add(new Employee(2, "Red", 25, "Male", 1500));
+    }
+
+    public List<Employee> listAll() {
+        return employees;
+    }
+
+    public Employee findById(Long id) {
+        return employees.stream()
+                .filter(employee -> employee.getId().equals(id))
+                .findFirst()
+                .orElseThrow(EmployeeNotFoundException::new);
+    }
+
+    public List<Employee> findByGender(String gender) {
+        return employees.stream()
+                .filter(employee -> employee.getGender().equals(gender))
+                .collect(Collectors.toList());
+    }
+}

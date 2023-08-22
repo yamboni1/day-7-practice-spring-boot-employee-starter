be48ba28a6bf4aa13f6aad8a786829ce067f21ea
Daphne Balane <balanedaphne20@gmail.com>
2023-08-22 15:53:23 +0800

    feat: added methods in Employee class

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/Employee.java
@@ -0,0 +1,37 @@
+package com.thoughtworks.springbootemployee.controller;
+
+public class Employee {
+    private Long id;
+    private String name;
+    private Integer age;
+    private String gender;
+    private Integer salary;
+
+    public Employee(long id, String name, int age, String gender, int salary) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.salary = salary;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+}

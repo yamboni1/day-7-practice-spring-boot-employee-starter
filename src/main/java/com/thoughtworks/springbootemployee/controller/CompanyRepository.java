d9d5a964dcde6117f1d9b8cda97ca888f69736e1
yamboni1 <ncyambot.work@gmail.com>
2023-08-22 20:56:37 +0800

    feat: added data for CompanyRepository and getCompanies method

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyRepository.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyRepository.java
@@ -1,4 +1,19 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CompanyRepository {
+    private static final List<Company> companies = new ArrayList<>();
+    static{
+        companies.add(new Company(1L, "Spring"));
+        companies.add(new Company(2L, "OOCL"));
+        companies.add(new Company(3L, "Google"));
+        companies.add(new Company(4L, "COSCO"));
+        companies.add(new Company(5L, "ITA"));
+    }
+
+    public static List<Company> getCompanies() {
+        return companies;
+    }
 }

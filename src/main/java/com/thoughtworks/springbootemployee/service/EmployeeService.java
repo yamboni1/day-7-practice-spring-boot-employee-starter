1543b94f431210dae98197595a8e4b2e8e38fad4
yamboni1 <ncyambot.work@gmail.com>
2023-08-23 21:11:51 +0800

    refactor:remove unused imports

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -6,7 +6,6 @@ import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 @Service

ea191758d8b4184852b664c50541ab0c34722d5a
yamboni1 <ncyambot.work@gmail.com>
2023-08-23 16:12:54 +0800

    test: should_return_created_employee_when_create_given_employee_service_and_employee_with_valid_age

--- src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
@@ -19,5 +19,23 @@ public class EmployeeServiceTest {
         employeeService = new EmployeeService(mockedEmployeeRepository);
 
     }
+    
+    @Test
+    void should_return_created_employee_when_create_given_employee_service_and_employee_with_valid_age() {
+    //given
+        Employee alice = new Employee(1L,"Alice", 24, "Female", 9000, 1L);
+        Employee savedEmployee = new Employee(1L,"Alice", 24, "Female", 9000, 1L);
+        when(mockedEmployeeRepository.addEmployee(alice)).thenReturn(savedEmployee);
+        //when
+        Employee employeeResponse = employeeService.create(alice);
+     
+     //then
+        assertEquals(savedEmployee.getId(),employeeResponse.getId());
+        assertEquals("Alice",employeeResponse.getName());
+        assertEquals(24,employeeResponse.getAge());
+        assertEquals("Female",employeeResponse.getGender());
+        assertEquals(9000,employeeResponse.getSalary());
+        assertEquals(1L,employeeResponse.getCompanyId());
+    }
 
 }

c35b85f4a93f473ede6717522a1b3d653ea58bfd
yamboni1 <ncyambot.work@gmail.com>
2023-08-23 20:26:14 +0800

    feat: added update employee

--- src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
+++ src/main/java/com/thoughtworks/springbootemployee/service/EmployeeService.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.springbootemployee.service;
 import com.thoughtworks.springbootemployee.exception.EmployeeCreateException;
+import com.thoughtworks.springbootemployee.exception.EmployeeInactiveStatusException;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 
@@ -14,11 +15,17 @@ public class EmployeeService {
         }
         return employeeRepository.addEmployee(employee);
     }
+    public Employee update(Long id, Employee employee){
+        if(!employee.isActive()){
+            throw new EmployeeInactiveStatusException();
+        }
+        return employeeRepository.updateEmployee(id,employee);
+    }
 
 
     public void delete(Long id) {
         Employee employeeToDelete = employeeRepository.findByEmployeeId(id);
         employeeToDelete.setActive(false);
-        employeeRepository.update(id,employeeToDelete);
+        employeeRepository.updateEmployee(id,employeeToDelete);
     }
 }
